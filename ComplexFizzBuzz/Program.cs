6aac8b8a9b7a758e8714eb1f1c1c5c2826f635bd
NicolasCoelho <nicolas_mansan@hotmail.com>
2025-02-28 11:21:56 -0300

    Schema for Complex FizzBuzz

--- ComplexFizzBuzz/Program.cs
+++ ComplexFizzBuzz/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ComplexFizzBuzz
+﻿using TwistedFizzBuzz;
+
+namespace ComplexFizzBuzz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var intialRange = -20;
+            var finalRange = 127;
+            var tokens = new List<KeyValuePair<int, string>>()
+            {
+                new KeyValuePair<int, string>(5, "Fizz"),
+                new KeyValuePair<int, string>(9, "Buzz"),
+                new KeyValuePair<int, string>(27, "Bar"),
+            };
+
+            new TwistedFizzBuzzEngine(intialRange, finalRange, tokens).DoFizzBuzz();
         }
     }
 }
--- TwistedFizzBuzz/TwistedFizzBuzzEngine.cs
+++ TwistedFizzBuzz/TwistedFizzBuzzEngine.cs
@@ -5,23 +5,46 @@ namespace TwistedFizzBuzz
 {
     public class TwistedFizzBuzzEngine : ITwistedFizzBuzz
     {
-        public readonly List<KeyValuePair<int, string>> KeyTokenList;
-        public readonly int MaxRange;
-
-        public TwistedFizzBuzzEngine()
-        {
-            KeyTokenList = new List<KeyValuePair<int, string>>()
+        public readonly IEnumerable<KeyValuePair<int, string>> KeyTokenList = new List<KeyValuePair<int, string>>()
             {
                 new KeyValuePair<int, string>(3, "Fizz"),
                 new KeyValuePair<int, string>(5, "Buzz")
             };
+        
+        public readonly int MinValue = 1;
+        public readonly int MaxValue = 100;
 
-            MaxRange = 100;
+        public TwistedFizzBuzzEngine() { }
+
+        public TwistedFizzBuzzEngine(IEnumerable<KeyValuePair<int, string>> tokens)
+        {
+            CheckTokens(tokens);
+
+            KeyTokenList = tokens;
+        }
+
+        public TwistedFizzBuzzEngine(int minValue, int maxValue)
+        {
+            CheckRange(minValue, maxValue);
+
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+
+        public TwistedFizzBuzzEngine(int minValue, int maxValue, IEnumerable<KeyValuePair<int, string>> tokens)
+        {
+            CheckRange(minValue, maxValue);
+            CheckTokens(tokens);
+
+            KeyTokenList = tokens;
+
+            MinValue = minValue;
+            MaxValue = maxValue;
         }
 
         public void DoFizzBuzz()
         {
-            for (var i = 1; i <= MaxRange; i++)
+            for (var i = MinValue; i <= MaxValue; i++)
             {
                 var text = new StringBuilder();
 
@@ -34,6 +57,17 @@ namespace TwistedFizzBuzz
 
                 Console.WriteLine(text);
             }
+        }
+
+        
+        private void CheckRange(int min, int max)
+        {
+            if (min > max || (min == 0 && max == 0)) throw new InvalidOperationException("Invalid range");
+        }
+
+        private void CheckTokens(IEnumerable<KeyValuePair<int, string>> tokens)
+        {
+            if (tokens.Count() == 0) throw new InvalidOperationException("Tokens list cannot be empty");
         }
     }
 }
